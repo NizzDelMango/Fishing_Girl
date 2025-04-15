74061817db1ec009a888725cce026596506248a5
whitemango04 <shinwltjd04@naver.com>
2025-04-15 09:31:32 +0900

    Fishing_Girl(Start_Scene)

--- Assets/Sc/BTN_Controller.cs
+++ Assets/Sc/BTN_Controller.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class BTN_Controller : MonoBehaviour
 {
     public GameObject storePanel;
+    public GameObject bucketPanel;
     public GameObject inventoryPanel;
 
     public Button storeButton;
+    public Button bucketButton;
     public Button inventoryButton;
 
     void Start()
     {
         storeButton.onClick.AddListener(ToggleStore);
+        bucketButton.onClick.AddListener(ToggleBucket);
         inventoryButton.onClick.AddListener(ToggleInventory);
 
         storePanel.SetActive(false);
@@ -24,13 +27,32 @@ public class BTN_Controller : MonoBehaviour
     {
         bool isActive = storePanel.activeSelf;
         storePanel.SetActive(!isActive);
-        if (!isActive) inventoryPanel.SetActive(false);
+        if (!isActive)
+        {
+            inventoryPanel.SetActive(false);
+            bucketPanel.SetActive(false);
+        }
+    }
+
+    void ToggleBucket()
+    {
+        bool isActive = bucketPanel.activeSelf;
+        bucketPanel.SetActive(!isActive);
+        if(isActive)
+        {
+            storePanel.SetActive(false);
+            inventoryPanel.SetActive(false);
+        }
     }
 
     void ToggleInventory()
     {
         bool isActive = inventoryPanel.activeSelf;
         inventoryPanel.SetActive(!isActive);
-        if (!isActive) storePanel.SetActive(false);
+        if (!isActive)
+        {
+            storePanel.SetActive(false);
+            bucketPanel.SetActive (false);
+        }
     }
 }
---
+++ Assets/Sc/StartBTN.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartBTN : MonoBehaviour
+{
+    public Button startButton;
+    void Start()
+    {
+        startButton.onClick.AddListener(ToggleStart);
+    }
+
+    void ToggleStart()
+    {
+        SceneManager.LoadScene("Test");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
