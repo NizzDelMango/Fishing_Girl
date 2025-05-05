fe7909223aa9fe3264084ca13dc22d128b3d3bc3
WhiteMango <shinwltjd04@naver.com>
2025-05-05 15:22:43 +0900

    그지같은 애드몹

---
+++ Assets/Sc/AdmobBanner.cs
@@ -0,0 +1,57 @@
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class AdmobBanner : MonoBehaviour
+{
+    public string bannerID = "";
+
+    BannerView _bannerView;
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            // This callback is called once the MobileAds SDK is initialized.
+            CreateBannerView();
+        });
+    }
+    public void CreateBannerView()
+    {
+        Debug.Log("Creating banner view");
+
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+
+        // Create a 320x50 banner at top of the screen
+        _bannerView = new BannerView(bannerID, AdSize.Banner , AdPosition.Bottom);
+        LoadAd();
+    }
+    public void LoadAd()
+    {
+        // create an instance of a banner view first.
+        if (_bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+    }
+    public void DestroyAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("Destroying banner view.");
+            _bannerView.Destroy();
+            _bannerView = null;
+        }
+    }
+}
