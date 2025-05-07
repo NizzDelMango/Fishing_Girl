828f92e9be07bf3a4b5da249b356b1422d9fe018
yuh2327015 <www9216@naver.com>
2025-05-07 13:38:55 +0900

    AdManager

---
+++ Assets/Sc/AdManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class AdManager : MonoBehaviour
+{
+    public static AdManager Instance; // 다른 스크립트에서 접근할 수 있게 싱글톤 사용
+    private RewardedAd rewardedAd;
+    private string adUnitId = "ca-app-pub-3940256099942544/5224354917"; // 테스트 광고 ID
+
+    private Action onAdSuccess; // 광고 성공 후 실행할 콜백
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => {
+            LoadRewardedAd();
+        });
+    }
+
+    public void LoadRewardedAd()
+    {
+        AdRequest request = new AdRequest();
+
+        RewardedAd.Load(adUnitId, request, (RewardedAd ad, LoadAdError error) =>
+        {
+            if (error != null || ad == null)
+            {
+                Debug.LogError("광고 로드 실패: " + error?.GetMessage());
+                return;
+            }
+
+            rewardedAd = ad;
+
+            rewardedAd.OnAdFullScreenContentClosed += () =>
+            {
+                Debug.Log("광고 닫힘");
+                LoadRewardedAd();
+            };
+
+            rewardedAd.OnAdFullScreenContentFailed += (AdError adError) =>
+            {
+                Debug.LogError("광고 실패: " + adError.GetMessage());
+            };
+        });
+    }
+
+    public void ShowRewardedAd(Action rewardCallback)
+    {
+        if (rewardedAd != null && rewardedAd.CanShowAd())
+        {
+            onAdSuccess = rewardCallback;
+
+            rewardedAd.Show((Reward reward) =>
+            {
+                Debug.Log("광고 보상 완료");
+                onAdSuccess?.Invoke(); // 낚싯대 지급
+            });
+        }
+        else
+        {
+            Debug.Log("광고 없음");
+        }
+    }
+}
--- Assets/Sc/StoreRodButton.cs
+++ Assets/Sc/StoreRodButton.cs
@@ -6,17 +6,29 @@ public class StoreRodButton : MonoBehaviour
     public int rodIndex; // 0: Bamboo, 1: Old, 2: Iron, 3: Master
     public int rodPrice;
     public Player_Stats playerStats;
+    public bool isAdRequired = false; // 이 낚싯대가 광고 시청으로만 얻는 것인지?
 
     void Start()
     {
-        // 버튼에 클릭 리스너 추가
         GetComponent<Button>().onClick.AddListener(BuyRod);
     }
 
     void BuyRod()
     {
-        if (playerStats != null)
+        if (playerStats == null) return;
+
+        if (isAdRequired)
         {
+            // 광고 시청 후 낚싯대 지급
+            AdManager.Instance.ShowRewardedAd(() =>
+            {
+                playerStats.BuyFishingRod(rodIndex, 0); // 가격은 0으로 처리
+                Debug.Log("광고 보상으로 낚싯대 지급!");
+            });
+        }
+        else
+        {
+            // 일반 골드로 구매
             playerStats.BuyFishingRod(rodIndex, rodPrice);
         }
     }
