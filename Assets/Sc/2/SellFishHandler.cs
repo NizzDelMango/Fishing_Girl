d8690a3bc8d00441744f8300e511f844c76ff1b0
WhiteMango <shinwltjd04@naver.com>
2025-06-14 23:49:01 +0900

    Bucket

--- Assets/Sc/2/SellFishHandler.cs
+++ Assets/Sc/2/SellFishHandler.cs
@@ -8,51 +8,33 @@ public class SellFishHandler : MonoBehaviour
     [Header("패널")]
     public GameObject Empty;
     public GameObject Full;
+
     void Start()
     {
         // 패널 상태 초기화
         Empty.SetActive(true);
-        Full.SetActive(false);
+        Full.SetActive(true);
 
         // 부모 객체에서 Animator 가져오기
         bucketAnimator = this.gameObject.GetComponent<Animator>();
 
-        // GameManager 가져오기 "SellAllFishi() 사용하기 위함"
+        // GameManager 가져오기 "SellAllFish() 사용하기 위함"
         gameManager = FindObjectOfType<GameManager>();
     }
-
-    //public void PlayBucketAnimation() // 애니메이션 동작 로직
-    //{
-    //    if (!this.gameObject.activeSelf)
-    //    {
-    //        // 꺼져 있으면 켜기
-    //        this.gameObject.SetActive(true);
-    //    }
-    //    else
-    //    {
-    //        // 켜져 있으면 애니메이션 실행 후 꺼짐
-    //        bucketAnimator.SetTrigger("Bucket_Touched");
-    //        StartCoroutine(DisableAfterAnimation());
-    //    }
-    //}
 
     public void SellAllFish()
     {
         if (gameManager != null)
         {
-            gameManager.SellAllFish();  // GameManager.cs 안에 있는 SellAllFish 호출
+            gameManager.SellAllFish();
         }
-
-        //// 애니메이션 실행 후 꺼짐
-        //bucketAnimator.SetTrigger("Bucket_Touched");
-        //StartCoroutine(DisableAfterAnimation());
     }
 
-    //private System.Collections.IEnumerator DisableAfterAnimation()
-    //{
-    //    // 애니메이션 길이만큼 대기 후 꺼짐
-    //    float animTime = bucketAnimator.GetCurrentAnimatorStateInfo(0).length;
-    //    yield return new WaitForSeconds(animTime);
-    //    this.gameObject.SetActive(false);
-    //}
+    public void PlayBucketAnimation()
+    {
+        if (bucketAnimator != null)
+        {
+            bucketAnimator.SetTrigger("Bucket_Touched");
+        }
+    }
 }
